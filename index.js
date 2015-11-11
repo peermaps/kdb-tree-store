d23de8c430d46158d14adc517451390eff34d13a
James Halliday <mail@substack.net>
2015-11-11 04:36:10 -0800

    first part of insertion

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ KDB.prototype.query = function (rquery, cb) {
         self._parseRegion(buf, pages, page[1])
         read()
       } else if (buf[0] === POINT) {
-        var pts = self._parsePoint(buf, page[1])
+        var pts = self._parsePoint(buf, page[1], q)
         if (pts.length === 0) read()
         for (var i = 0; i < pts.length; i++) results.push(pts[i])
       }
@@ -80,7 +80,7 @@ KDB.prototype._parseRegion = function (buf, pages, depth) {
   }
 }
 
-KDB.prototype._parsePoint = function (buf, depth) {
+KDB.prototype._parsePoint = function (buf, depth, query) {
   var self = this
   var npoints = buf.readUInt16BE(1)
   var len = self.types.length
@@ -115,8 +115,7 @@ function normq (q) {
     for (var i = 0; i < q.length; i++) {
       if (Array.isArray(q[i])) {
         query.push(q[i])
-      }
-      else {
+      } else {
         query.push([ q[i], q[i] ]) // min, max
       }
     }
@@ -130,3 +129,109 @@ function normq (q) {
   return { query: query, filter: filter }
   function ftrue () { return true }
 }
+
+KDB.prototype.insert = function (pt, cb) {
+  var self = this
+  if (!cb) cb = noop
+  var query = []
+  for (var i = 0; i < pt.length - 1; i++) query.push(pt[i], pt[i])
+  var pages = [ [ self.root, 0 ] ]
+  var region = null, regionPage = null
+
+  ;(function read () {
+    if (pages.length === 0) return results.push(null)
+    var page = pages.shift()
+    self.store.get(page[0], function (err, buf) {
+      if (err) return cb(err)
+      if (buf.length === 0 && self.root === page[0]) {
+        var pbuf = self._createPointPage()
+        self._addPoint(pbuf, pt)
+        return self.store.set(self.root, pbuf, cb)
+      } else if (buf.length === 0) {
+        return cb(new Error('empty page: ' + page[0]))
+      }
+
+      if (buf[0] === REGION) {
+        self._parseRegion(buf, pages, page[1])
+        region = buf
+        regionPage = page[0]
+        read()
+      } else if (buf[0] === POINT) {
+        if (self._addPoint(buf, pt)) {
+          return self.store.set(page[0], buf, cb)
+        }
+        var sp = self._splitPointPage(buf, page[1])
+        if (self._addRegion(region, sp.left, sp.right)) {
+          // ...save things...
+        } else {
+          // split region, re-order
+        }
+      }
+    })
+  })()
+}
+
+KDB.prototype._splitPointPage = function (buf, depth) {
+  var self = this
+  var len = self.types.length
+  var npoints = buf.readUInt16BE(1)
+  var d = depth % len
+  var points = [], coords = []
+  for (var i = 0; i < npoints; i++) {
+    var pt = []
+    for (var j = 0; j < len; j++) {
+      var t = self.types[j % len]
+      if (t === 'float32') {
+        var p = buf.readFloat32BE(offset)
+        offset += 4
+      } else throw new Error('unsupported type: ' + t)
+      pt.push(p)
+    }
+    points.push(pt)
+    coords.push(pt[d])
+  }
+  var pivot = median(coords)
+  var left = [], right = []
+  for (var i = 0; i < npoints; i++) {
+    if (points[i][d] < pivot) left.push(points[i])
+    else right.push(points[i])
+  }
+  return { left: left, right: right }
+}
+
+KDB.prototype._createPointPage = function () {
+  var self = this
+  var buf = new Buffer(self.size)
+  buf[0] = POINT
+  buf.writeUInt16BE(0, 1)
+  return buf
+}
+
+KDB.prototype._addPoint = function (buf, pt) {
+  var self = this
+  var npoints = buf.readUInt16BE(1)
+  var len = self.types.length
+  var offset = 3
+
+  for (var i = 0; i < npoints; i++) {
+    for (var j = 0; j < len; j++) {
+      var t = self.types[j]
+      if (t === 'float32') {
+        offset += 4
+      } else throw new Error('unknown type: ' + t)
+    }
+  }
+  for (var j = 0; j < pt.length - 1; j++) {
+    var t = self.types[j]
+    if (t === 'float32') {
+      if (offset > buf.length) return false // overflow
+      buf.writeFloat32BE(pt[j], offset)
+      offset += 4
+    } else throw new Error('unknown type: ' + t)
+  }
+  if (offset > buf.length) return false // overflow
+  buf.writeUInt32BE(pt[j], offset)
+  return true // no overflow
+}
+
+function noop () {}
