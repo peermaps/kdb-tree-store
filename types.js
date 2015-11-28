038d2bd64f4b449b87c0c21e7dac169c6f9e0de1
James Halliday <mail@substack.net>
2015-11-28 01:45:02 -0600

    more types

--- types.js
+++ types.js
@@ -3,7 +3,7 @@ var FLT = almostEqual.FLT_EPSILON
 var DBL = almostEqual.DBL_EPSILON
 
 module.exports = function (t) {
-  if (t === 'float32' || t === 'f32' || t === 'f') {
+  if (/^(f|f32|float32)$/.test(t)) {
     return {
       read: function (buf, offset) {
         return buf.readFloatBE(offset)
@@ -19,13 +19,13 @@ module.exports = function (t) {
         gte: function (a, b) { return a >= b || almostEqual(a, b, FLT, FLT) }
       }
     }
-  } else if (t === 'float64' || t === 'double' || t === 'f64' || t === 'd') {
+  } else if (/^(d|f64|double|float64)$/.test(t)) {
     return {
       read: function (buf, offset) {
-        return buf.readFloatBE(offset)
+        return buf.readDoubleBE(offset)
       },
       write: function (buf, value, offset) {
-        return buf.writeFloatBE(value, offset)
+        return buf.writeDoubleBE(value, offset)
       },
       size: 8,
       cmp: {
@@ -35,5 +35,77 @@ module.exports = function (t) {
         gte: function (a, b) { return a >= b || almostEqual(a, b, DBL, DBL) }
       }
     }
+  } else if (/^u(|i|int)?8$/.test(t)) {
+    return {
+      read: function (buf, offset) {
+        return buf.readUInt8BE(offset)
+      },
+      write: function (buf, value, offset) {
+        return buf.writeUInt8BE(value, offset)
+      },
+      size: 1,
+      cmp: icmp
+    }
+  } else if (/^u(|i|int)?16$/.test(t)) {
+    return {
+      read: function (buf, offset) {
+        return buf.readUInt16BE(offset)
+      },
+      write: function (buf, value, offset) {
+        return buf.writeUInt16BE(value, offset)
+      },
+      size: 2,
+      cmp: icmp
+    }
+  } else if (/^u(|i|int)?32$/.test(t)) {
+    return {
+      read: function (buf, offset) {
+        return buf.readUInt32BE(offset)
+      },
+      write: function (buf, value, offset) {
+        return buf.writeUInt32BE(value, offset)
+      },
+      size: 4,
+      cmp: icmp
+    }
+  } else if (/^s?(|i|int)?8$/.test(t)) {
+    return {
+      read: function (buf, offset) {
+        return buf.readInt8BE(offset)
+      },
+      write: function (buf, value, offset) {
+        return buf.writeInt8BE(value, offset)
+      },
+      size: 1,
+      cmp: icmp
+    }
+  } else if (/^s?(|i|int)?16$/.test(t)) {
+    return {
+      read: function (buf, offset) {
+        return buf.readInt16BE(offset)
+      },
+      write: function (buf, value, offset) {
+        return buf.writeInt16BE(value, offset)
+      },
+      size: 2,
+      cmp: icmp
+    }
+  } else if (/^s?(|i|int)?32/.test(t)) {
+    return {
+      read: function (buf, offset) {
+        return buf.readInt32BE(offset)
+      },
+      write: function (buf, value, offset) {
+        return buf.writeInt32BE(value, offset)
+      },
+      size: 4,
+      cmp: icmp
+    }
   }
 }
+
+var icmp = { lt: lt, lte: lte, gt: gt, gte: gte }
+function lt (a, b) { return a < b }
+function lte (a, b) { return a <= b }
+function gt (a, b) { return a > b }
+function gte (a, b) { return a >= b }
